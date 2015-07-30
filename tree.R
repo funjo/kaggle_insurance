c02a272b85fbcc1bdbe0dbf6413c6afa8f7c978b
Fangzhou Cheng <chengfzchn@gmail.com>
2015-07-30 16:56:25 -0400

    xgboost late stop + random forests

--- tree.R
+++ tree.R
@@ -41,18 +41,38 @@ mtest = predict(dummies, newdata = mtest)
 param <- list("objective" = "reg:linear",
               "nthread" = 8,
               "verbose"= 0,
-              "silent" = 1)
+              "silent" = 1,
+              "eta" = .01,
+              "max.depth" = 7,
+              "min_child_weight" = 5,
+              "scale_pos_weight" = 1.0,
+              "subsample" = 0.8)
 
 cat("Training model - Xgboost\n") 
 # Fit the model
-xgb.fit = xgboost(param=param, data = mtrain, label = y,
-                  nrounds=2000, eta = .01, max_depth = 7, 
-                  min_child_weight = 5, scale_pos_weight = 1.0, subsample=0.8) 
+# xgb.fit = xgboost(param=param, data = mtrain, label = y, nrounds=2000) 
+# pred_xgboost <- predict(xgb.fit, mtest)
+
+# early stopping
+dtrain <- xgb.DMatrix(mtrain[-c(1:5000),], label = y[-c(1:5000)])
+dtest <- xgb.DMatrix(mtrain[1:5000,], label = y[1:5000])
+watchlist <- list(eval = dtest, train = dtrain)
+xgb.fit <- xgb.train(param = param, data = dtrain, label = y, nround = 2000,
+                     watchlist = watchlist, early.stop.round = 200, maximize = FALSE)
 pred_xgboost <- predict(xgb.fit, mtest)
+
+cat("Training model - Xgboost no.2\n") 
+dtrain <- xgb.DMatrix(mtrain[-c(nrow(mtrain)-4999:nrow(mtrain)),],
+                      label = y[-c(length(y)-4999:length(y))])
+dtest <- xgb.DMatrix(mtrain[nrow(mtrain)-4999:nrow(mtrain),],
+                     label = y[length(y)-4999:length(y)])
+watchlist <- list(eval = dtest, train = dtrain)
+xgb.fit2 <- xgb.train(param = param, data = dtrain, label = y, nround = 2000,
+                     watchlist = watchlist, early.stop.round = 200, maximize = FALSE)
+pred_xgboost2 <- predict(xgb.fit, mtest)
 
 cat("Predicting Hazard...\n")
 # Predict Hazard for the test set
 submission <- data.frame(Id=test$Id)
-submission$Hazard <- pred_rf + pred_xgboost
-write_csv(submission, "submit_rf+xgboost+ada.csv")
-
+submission$Hazard <- (pred_rf + pred_xgboost + pred_xgboost2)/3
+write_csv(submission, "submit_rf+xgboost+stop100+cross.csv")
